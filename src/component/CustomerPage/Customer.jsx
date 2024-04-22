e5ac2d82ddc391ebd7ecf9237db5b415d08c318a
ravishansenevirathna <travishan55@gmail.com>
2024-04-23 00:23:34 +0530

    17th commit

--- src/component/CustomerPage/Customer.jsx
+++ src/component/CustomerPage/Customer.jsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 import GroupsIcon from '@mui/icons-material/Groups';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import './Customer.css';  // Import the CSS file
 
 
 
@@ -47,6 +48,7 @@ export default function Customer() {
 
 
     return(
+      <div className="customer-container">
         <div style={{ height: 400, width: '100%' }}>
           <Box sx={{ boxShadow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '12px 15px', borderBottom: '1px solid #ddd',   background: `linear-gradient(to right, #d3d3e3, #c0c0d9)`,  }}>
                 <GroupsIcon sx={{ fontSize: 40, marginRight: 10, color:"primary.main" }}  />
@@ -69,6 +71,7 @@ export default function Customer() {
           pageSizeOptions={[5, 10]}
           checkboxSelection
         />
+      </div>
       </div>
     )
 }
